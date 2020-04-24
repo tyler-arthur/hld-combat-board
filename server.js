38743b2f602d3dd28f3012ecdd08feae4809791c
Swedish <tarthurf@yahoo.com>
2020-04-24 12:43:43 -0700

    Created basic file structure, set homepage route, added background, added draggable image

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
